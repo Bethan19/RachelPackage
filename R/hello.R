0c47852db1fc0427e8ace8892abfebdf3b69cfe9
Bethan19 <2173737m@student.gla.ac.uk>
2019-11-11 13:41:51 +0000

    ab

--- R/hello.R
+++ R/hello.R
@@ -3,7 +3,7 @@
 # This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
-# You can learn more about package authoring with RStudo at:
+# You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
 #
